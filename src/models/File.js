5499c791d343dd156187a13db6ea820338434131
thayron <thayronf3@gmail.com>
2021-03-04 06:58:19 -0300

    Foi criado o model para inserir, atualizar e buscar os dados dos arquivos armazenados no servidor. a rota api/upload e reponsavel por atualizar os dados do arquivo caso o mesmo ja exista no banco de dados ou criar um novo caso contrario. (atualiza os dados no banco e apaga o arquivo do servidor)

--- src/models/File.js
+++ src/models/File.js
@@ -4,18 +4,44 @@ class File {
 
  async select(valores) {
     try {
-      const { id_user, tipo } = valores;
-      console.log(tipo);
-      const sql = `SELECT * FROM arquivos WHERE id_user = ? and tipo = ?`;
-      const user = await connection.promise().query(sql, [id_user, tipo]);
+      const { id_user, typeFile, typeDoc } = valores;
+      const sql = `SELECT * FROM arquivos WHERE id_user = ? and typeDoc = ? and typeFile = ?`;
+      const user = await connection.promise().query(sql, [id_user, typeDoc, typeFile]);
         if (user[0].length === 0) {
           return false;
         }
-        return user[0]
+        return user[0][0]
     } catch (err) {
       console.log(err);
     }
   };
+
+  async update (id, valores) {
+    try {
+      const sql = `UPDATE arquivos SET ? WHERE id=?`;
+      const file = await connection.promise().query(sql, [valores, id]);
+        if (file[0].length === 0) {
+          return false;
+        }
+        return file[0]
+    } catch (err) {
+      console.log(err);
+    }
+
+  }
+
+  async insert (req) {
+    try {
+      const sql = 'INSERT INTO arquivos SET ?';
+
+      const fileResponse = await connection.promise().query(sql, req);
+      if (fileResponse[0].length === 0) return false;
+      return req
+    } catch (err) {
+      console.log(err);
+    }
+  }
 };
+
 
 module.exports = new File;
--- src/service/fileStoregeServer.js
+++ src/service/fileStoregeServer.js
@@ -1,59 +1,45 @@
 const { Router } = require('express');
-const path = require('path');
+const paths = require('path');
 const fs = require('fs');
 const multer = require('multer');
 const uploadConfig = require('../config/upload');
 const upload = multer(uploadConfig);
 const filerUser = require('../models/File')
 const fileStoregeServer = Router();
+const FindUser = require('../models/User');
 
 fileStoregeServer.patch('/upload', upload.single('files'), async (request, response) => {
 
   try {
-    const { document } = request.body;
-    const { filename } = request.file;
-    
-    if(!document || !filename) {
-      response.status(400).json({error: 'file e corpos são obrigatorios'});
+    const { document, id_user } = request.body;
+    const { filename, originalname, mimetype, path } = request.file;
+
+    const valores = { id_user: id_user, typeDoc: document, typeFile: mimetype.split('/')[1] }
+   
+    const user = await filerUser.select(valores);
+
+    const isertFile = { id_user, filename, typeFile: mimetype.split('/')[1], typeDoc: document,  originalname, path }
+
+    if (!user) {
+      await filerUser.insert(isertFile)
+      return response.status(201).json({"sucess": "arquivo gravado no banco de dados"})
     }
-    
-    const valores = {id_user: '1', tipo: document}
-     /**
-     * Verifica se o usuario existe  no banco de dados caso contratio e feito a inserção do arquivo no banco de dados
-     */
-    const user =  await filerUser.select(valores)
 
-    // inserir no banco de dados
-    // if (!user) {
-    //  return  response.status('criar usuario')
+    const caminho = paths.join(uploadConfig.directory, user.filename)
 
-    // }
-
-
-    /**
-     * é feito uma validação para verificar se o arquivo contem na pasta tmp
-     */
-    const caminho = path.join(uploadConfig.directory, 'filename')
-    // const existes = await fs.promises.open
-
-   const existes =  fs.existsSync(caminho);
-  console.log(existes);
-    /**
-     * Se o arquivo não existir e armazendo na pasta temp o arquivo e inserido os dados do arquivo no banco de dados
-     */
+    const existes = fs.existsSync(caminho);
     if (!existes) {
-      console.log('....................');
-      return  response.status(201).json({error: 'arquivo criado'});
+      await filerUser.insert(isertFile);
+      return response.status(201).json({ error: 'arquivo criado' });
     }
-    /**
-     * se o arquivo existir e apagado o arquivo anterior e armazenado o novo na pasta temp
-     * 
-     * 
-     * ***** FAZER UM UPDATE NA TABELA COM AS NOVAS INFORMAÇOES DO ARUIVO ****
-     */
-    
+
+    if (existes) {
+      fs.promises.unlink(caminho);
+      filerUser.update(user.id, {filename, typeFile: mimetype.split('/')[1], typeDoc: document, originalname, path})
+      return response.status(400).json({ error: 'arquivo trocado' });
+    }
+
     fs.promises.unlink(caminho);
-    response.status(400).json({error: 'arquivo trocado'});
 
   } catch (err) {
     console.log(err);
